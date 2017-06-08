62c1f739f547d7ae21e6317dfa60b7709f576ce5
Till DE ROQUEFEUIL <tde-roqu@e2r6p12.42.fr>
2017-06-08 15:45:30 +0200

    twig

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const Twig = require("twig")
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!!')
+})
+
+app.get('/twig', function (req, res) {
+  res.render('index.twig', {
+    message : "Hello World"
+  });
+})
+
+app.get('/test/:nbr', function (req, res) {
+  var response = "Vous etes sur la page test numero " + req.params.nbr + "!"
+  res.send(response)
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable!');
+});
