e2fd26248e9a22719ad62a545086225011d2425b
Till DE ROQUEFEUIL <tde-roqu@e2r6p12.42.fr>
2017-06-08 17:48:14 +0200

    setup

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Twig = require("twig")
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
   res.send('Hello World!!')
 })
