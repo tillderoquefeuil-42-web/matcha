1bfb4af76d44b9bbcbc89a252045e8f9c9e4cea8
tillderoquefeuil <tillderoquefeuil@gmail.com>
2019-02-21 18:34:33 +0100

    test update (tags)

--- controllers/account/test.js
+++ controllers/account/test.js
@@ -3,35 +3,49 @@ const passwordHash = require("password-hash");
 const User = require('../../database/models/user.js');
 const UserRepo = require('../../database/repositories/user.js');
 
+const TagRepo = require('../../database/repositories/tag.js');
+
+
+const required = [
+    'firstname', 'lastname', 'username', 'email', 'password', 
+    'gender', 'birthday', 'language'
+];
 
 exports.createTestAccount = function(data) {
 
-    const required = [
-        'firstname', 'lastname', 'username', 'email', 'password', 
-        'gender', 'birthday', 'see_f', 'see_m', 'see_nb', 'language'
-    ];
+    return new Promise((resolve, reject) => {
 
-    for (var i in required) {
-        if (!data[required[i]]) {
-            return false;
+        for (var i in required) {
+            if (!data[required[i]]) {
+                return reject("data missging : " + required[i]);
+            }
         }
-    }
-    
-    var user = {
-        firstname   : data.firstname,
-        lastname    : data.lastname,
-        username    : data.username,
-        email       : data.email,
-        language    : data.language,
-        gender      : data.gender,
-        birthday    : data.birthday,
-        see_f       : data.see_f,
-        see_m       : data.see_m,
-        see_nb      : data.see_nb,
-        password    : passwordHash.generate(data.password),
-        valide      : true,
-        providers   : ['local', 'test']
-    }
+        
+        var user = {
+            firstname   : data.firstname,
+            lastname    : data.lastname,
+            username    : data.username,
+            email       : data.email,
+            language    : data.language,
+            gender      : data.gender,
+            birthday    : data.birthday,
+            see_f       : data.see_f,
+            see_m       : data.see_m,
+            see_nb      : data.see_nb,
+            password    : passwordHash.generate(data.password),
+            valide      : true,
+            providers   : ['local', 'test']
+        }
 
-    return UserRepo.createOne(user);
+        UserRepo.createOne(user)
+        .then(_user => {
+            TagRepo.updateUserTags(_user, data.tags)
+            .then(_user => {
+                return resolve(_user);
+            });
+        }).catch(error => {
+            return reject(error);
+        })
+
+    });
 };
